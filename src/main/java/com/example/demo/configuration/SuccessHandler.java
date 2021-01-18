ee25cd9cefe44426fc860b5d0cb7967b6437c376
michals96 <michal.stefaniuk@motorolasolutions.com>
2021-01-18 23:17:03 +0100

    Token authorization filter and success handler

---
+++ src/main/java/com/example/demo/configuration/SuccessHandler.java
@@ -0,0 +1,36 @@
+package com.example.demo.configuration;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+
+public class SuccessHandler implements AuthenticationSuccessHandler {
+    @Value("${jwt.secret}")
+    public String secret;
+
+    @Value("${jwt.expirationTime}")
+    public long expirationTime;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) throws IOException, ServletException {
+        UserDetails principal = (UserDetails) authentication.getPrincipal();
+        String token = JWT.create()
+                .withSubject(principal.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + expirationTime))
+                .sign(Algorithm.HMAC256(secret));
+        response.getOutputStream().print("{\"token\": \"" + token + "\"}");
+
+        // Return token in header
+        response.addHeader("Authorization", "Bearer " + token);
+    }
+}

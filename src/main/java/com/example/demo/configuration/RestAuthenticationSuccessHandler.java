f9b432f5e687c2bf363b357a1786aeab42ac1d21
michals96 <michal.stefaniuk9@gmail.com>
2021-01-25 23:54:58 +0100

    Successfull token generation

--- src/main/java/com/example/demo/configuration/RestAuthenticationSuccessHandler.java
+++ src/main/java/com/example/demo/configuration/RestAuthenticationSuccessHandler.java
@@ -1,6 +1,10 @@
 package com.example.demo.configuration;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -8,12 +12,31 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
 
 @Component
 public class RestAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
-    @Override
+    /* @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws IOException, ServletException {
         clearAuthenticationAttributes(request);
+    }*/
+
+    @Value("${jwt.secret}")
+    String secret;
+
+    @Value("${jwt.expirationTime}")
+    long expirationTime;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) throws IOException, ServletException {
+        UserDetails principal = (UserDetails) authentication.getPrincipal();
+        String token = JWT.create()
+                .withSubject(principal.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + expirationTime))
+                .sign(Algorithm.HMAC256(secret));
+        response.addHeader("Authorization", "Bearer " + token);
+       // response.getOutputStream().print("{\"token\": \"" + token + "\"}");
     }
 }

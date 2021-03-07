251985bb640b4490c30abcd455b07ac7c0dcfb04
michals96 <michal.stefaniuk9@gmail.com>
2021-03-07 18:37:37 +0100

    Form for inputting city name

--- demo-app/src/components/HomePage.js
+++ demo-app/src/components/HomePage.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
 
-class homepage extends Component{
-    render(){
-        return(
-            <div> Welcome to home page! </div>
-        );
-    }
+class homepage extends Component {
+  render() {
+    return (
+      <div className="todoListMain">
+        <div className="header">
+          <form>
+            <input placeholder="enter task"></input>
+            <button type="submit">add</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 export default homepage;
